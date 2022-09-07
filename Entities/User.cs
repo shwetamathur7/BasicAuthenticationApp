b0d574f3e3719b37bb9dd586c71d60eadb7f3f4e
shwetamathur7 <shwetamathur@microsoft.com>
2022-09-07 15:14:28 +0530

    Major code

---
+++ Entities/User.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace WebApi.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}
